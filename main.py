10206b5fd9bceb65a72efdf32f2fe7bd17278cde
howa <howa.wong@gmail.com>
2017-12-29 15:43:45 +0800

    Add process_query command

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import os
 import telebot
 from telebot import types
 from data import districts as districts_data
+import json
+import datetime
 
 bot = telebot.TeleBot(os.environ.get('API_KEY'))
 checking_dict = {}
+schedule = json.loads(open("output/488_201712.json", "r").read())
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
@@ -39,11 +42,22 @@ def process_district(message):
         places.append(district)
 
     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    print(places)
     markup.add(*places)
     msg = bot.reply_to(message, '選擇運動場', reply_markup=markup)
     bot.register_next_step_handler(msg, process_area)
 
 def process_area(message):
-    bot.reply_to(message, 'Test: ' + message.text)
+    today = datetime.date.today().strftime('%Y-%m-%d')    
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    markup.add(*["今日", "今星期", "兩星期"])
+    msg = bot.reply_to(message, '選擇時間', reply_markup=markup)
+    bot.register_next_step_handler(msg, process_query)
+
+
+def process_query(message):
+    print(message)
+    today = datetime.date.today().strftime('%Y-%m-%d')    
+    bot.reply_to(message, "沒有數據")
 
 bot.polling()
