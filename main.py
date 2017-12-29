b82e49bb1b34ba546df42907e665d1a4d096837f
Carmen <carmen@oursky.com>
2017-12-29 17:08:12 +0800

    Load the schedule json and support today data

--- main.py
+++ main.py
@@ -4,10 +4,46 @@ from telebot import types
 from data import districts as districts_data
 import json
 import datetime
+import logging
+
+log = logging.getLogger(__name__)
 
 bot = telebot.TeleBot(os.environ.get('API_KEY'))
-checking_dict = {}
+schedule_dict = {}
 schedule = json.loads(open("output/488_201712.json", "r").read())
+
+
+def get_district_and_place_by_id(pid):
+    for d in districts_data:
+        for p in d['places']:
+            if p['id'] == pid:
+                return p['name'], d['district']
+    return None, None
+
+
+def get_id_by_place_name(name):
+    for d in districts_data:
+        for p in d['places']:
+            if p['name'] == name:
+                return p['id']
+    return None
+
+
+def load_schedule_to_dict():
+    for s in schedule:
+        date = s["date"]
+        pid = s["id"]
+        place, district = get_district_and_place_by_id(pid)
+        if place is None:
+            continue
+        schedule_dict.setdefault(date, {
+            'district': set(),
+            'places': set(),
+            'schedule': {}
+        })
+        schedule_dict[date]['district'].add(district)
+        schedule_dict[date]['places'].add(place)
+        schedule_dict[date]['schedule'][pid] = s['schedule']
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
@@ -17,47 +53,80 @@ def send_welcome(message):
 
 @bot.message_handler(commands=['check'])
 def start_checking(message):
-    chat_id = message.chat.id
-    checking_dict[chat_id] = {}
+    today = datetime.date.today().strftime('%Y-%m-%d')
+
+    # load data
+    if today not in schedule_dict:
+        load_schedule_to_dict()
+
+    districts_set = schedule_dict[today]['district']
+
+    districts = [d['district'] for d in districts_data if d['district'] in districts_set]
+    if not len(districts):
+        log.info('Cannot found districts')
+        bot.reply_to(message, "沒有數據")
+        return
 
     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
-    markup.add(*[d['district'] for d in districts_data])
+    markup.add(*districts)
     msg = bot.reply_to(message, '選擇地區', reply_markup=markup)
     bot.register_next_step_handler(msg, process_district)
 
 
 def process_district(message):
-    # save district
     district = message.text
-    chat_id = message.chat.id
-    checking_dict[message.chat.id]['district'] = district
-
     # ask areas
-    places = next((d['places'] for d in districts_data if d['district'] == district), None)
-    if places is None:
+    places_dict = next((d['places'] for d in districts_data if d['district'] == district), None)
+    if places_dict is None:
         raise Exception()
+
+    today = datetime.date.today().strftime('%Y-%m-%d')
+    places_set = schedule_dict[today]['places']
+
+    places = [p['name'] for p in places_dict if p['name'] in places_set]
 
     # handle no places
     if not len(places):
-        places.append(district)
+        log.error('Cannot found places')
+        bot.reply_to(message, "沒有數據")
+        return
 
     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
-    print(places)
+    # print(places)
     markup.add(*places)
     msg = bot.reply_to(message, '選擇運動場', reply_markup=markup)
-    bot.register_next_step_handler(msg, process_area)
-
-def process_area(message):
-    today = datetime.date.today().strftime('%Y-%m-%d')    
-    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
-    markup.add(*["今日", "今星期", "兩星期"])
-    msg = bot.reply_to(message, '選擇時間', reply_markup=markup)
     bot.register_next_step_handler(msg, process_query)
+    # bot.register_next_step_handler(msg, process_area)
+
+# def process_area(message):
+#     today = datetime.date.today().strftime('%Y-%m-%d')
+#     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+#     markup.add(*["今日", "今星期", "兩星期"])
+#     msg = bot.reply_to(message, '選擇時間', reply_markup=markup)
+#     bot.register_next_step_handler(msg, process_query)
 
 
 def process_query(message):
-    print(message)
-    today = datetime.date.today().strftime('%Y-%m-%d')    
-    bot.reply_to(message, "沒有數據")
+    place = message.text
+    pid = get_id_by_place_name(place)
+
+    if pid is None:
+        log.error('Cannot found place id')
+        bot.reply_to(message, "沒有數據")
+        return
+
+    today = datetime.date.today().strftime('%Y-%m-%d')
+    schedule = schedule_dict[today]['schedule'].get(pid)
+
+    if schedule is None:
+        log.error('Cannot found schedule')
+        bot.reply_to(message, "沒有數據")
+        return
+
+    result_text = ""
+    for s in schedule:
+        result_text += s['time'] + ' ' + s['status'] + '\n'
+
+    bot.reply_to(message, result_text)
 
 bot.polling()
